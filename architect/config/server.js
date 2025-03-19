e728e33eda79551f084f99f477f572b00d760619
naveenbishwas <naveenbishwas4@gmail.com>
2025-03-19 16:23:26 +0530

    strapi new node

--- architect/config/server.js
+++ architect/config/server.js
@@ -1,19 +1,10 @@
-// module.exports = ({ env }) => ({
-//   host: env('HOST', '0.0.0.0'),
-//   port: env.int('PORT', 1337),
-//   app: {
-//     keys: env.array('APP_KEYS'),
-//   },
-//   webhooks: {
-//     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-//   },
-// });
-
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("RENDER_EXTERNAL_URL", "https://architect-3cto.onrender.com"),
   app: {
     keys: env.array("APP_KEYS"),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
   },
 });
