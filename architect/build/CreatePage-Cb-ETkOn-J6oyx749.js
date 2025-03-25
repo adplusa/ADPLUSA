60c67d18dbb35edc2885f6dce9fe8d88cdb5a24a
naveenbishwas <naveenbishwas4@gmail.com>
2025-03-25 11:09:35 +0530

    change module

--- architect/build/CreatePage-Cb-ETkOn-J6oyx749.js
+++
@@ -1,1 +0,0 @@
-import{a8 as r,j as e,P as o}from"./strapi-CO0p2vBv.js";import{E as a}from"./EditPage-C44OaxjN-CsDegqCS.js";import"./selectors-Dej_6h4x-BacdedyS.js";import"./useWebhooks-DzJ3X-hd-BxKGsRpR.js";const c=()=>{const s=r(t=>t.admin_app.permissions.settings?.webhooks.create);return e.jsx(o.Protect,{permissions:s,children:e.jsx(a,{})})};export{a as CreatePage,c as ProtectedCreatePage};
--- architect/build/useOnce-EHSWW7FY-Dm0LqP0d.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./strapi-CO0p2vBv.js";const r=e=>s.useEffect(e,t),t=[];export{r as u};
