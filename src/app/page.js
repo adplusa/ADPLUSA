8afed3272df6252d58c26e46a59fce8c0d1af342
naveenbishwas <naveenbishwas4@gmail.com>
2025-06-16 15:40:09 +0530

    arrow

--- src/app/page.js
+++ src/app/page.js
@@ -946,12 +946,12 @@ export default function Home() {
                         width="16"
                         height="16"
                         fill="currentColor"
-                        className="bi bi-arrow-left"
+                        className="bi bi-chevron-left"
                         viewBox="0 0 16 16"
                       >
                         <path
-                          fillRule="evenodd"
-                          d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+                          fill-rule="evenodd"
+                          d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"
                         />
                       </svg>
                     </button>
@@ -1027,12 +1027,12 @@ export default function Home() {
                         width="16"
                         height="16"
                         fill="currentColor"
-                        className="bi bi-arrow-right"
+                        className="bi bi-chevron-right"
                         viewBox="0 0 16 16"
                       >
                         <path
-                          fillRule="evenodd"
-                          d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+                          fill-rule="evenodd"
+                          d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"
                         />
                       </svg>
                     </button>
---
+++ src/app/serviceData.js
@@ -0,0 +1,15 @@
+// Example: servicesData.js
+export const services = [
+  {
+    id: 1,
+    title: "Service One",
+    slug: "service-one",
+    image: "/images/service1.jpg",
+  },
+  {
+    id: 2,
+    title: "Service Two",
+    slug: "service-two",
+    image: "/images/service2.jpg",
+  },
+];
