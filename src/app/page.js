ae76ed99b69d666a78d6a7471f149e97e62d357a
naveenbishwas <naveenbishwas4@gmail.com>
2025-09-20 13:24:56 +0530

    remove the quotes in projectId

--- src/app/page.js
+++ src/app/page.js
@@ -1454,62 +1454,6 @@ export default function Home() {
                 </a>
               </div>
 
-              {/* <div className="enquire">
-                <button onClick={() => setShowForm(true)}>Enquire Now</button>
-              </div>
-              {showForm && (
-                <div
-                  className="enquiry-overlay"
-                  onClick={() => setShowForm(false)}
-                >
-                  <div
-                    className="enquiry-container"
-                    onClick={(e) => e.stopPropagation()} // Prevent close on form click
-                  >
-                    <div className="enquiry-box">
-                      <div
-                        className="close-icon"
-                        onClick={() => setShowForm(false)}
-                      >
-                        ✕
-                      </div>
-                      <h2 className="title">Quick Query</h2>
-                      <p className="subtitle">
-                        If you have any queries, we will be pleased to assist
-                        you.
-                      </p>
-                      <form>
-                        <input
-                          type="text"
-                          placeholder="Name"
-                          className="form-input"
-                        />
-                        <input
-                          type="text"
-                          placeholder="Mobile No."
-                          className="form-input"
-                        />
-                        <select className="form-input">
-                          <option>Select Type</option>
-                          <option>General</option>
-                          <option>Support</option>
-                          <option>Sales</option>
-                        </select>
-                        <textarea
-                          placeholder="Query"
-                          className="form-input"
-                          rows="3"
-                        ></textarea>
-
-                        <button type="submit" className="submit-button">
-                          Submit
-                        </button>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              )} */}
-
               {showScrollUp && (
                 <div className="upward" onClick={upwardHandler}>
                   <svg
