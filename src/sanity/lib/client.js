f15db0cf8f00c04b524a094da7b14334105f1a67
naveenbishwas <naveenbishwas4@gmail.com>
2025-09-26 11:56:27 +0530

    iid

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,23 +1,10 @@
 import { createClient } from "next-sanity";
 import { apiVersion, dataset, projectId } from "../env";
 
-// export const client = createClient({
-//   projectId: "5ippxm43",
-
-//   dataset: "production",
-//   apiVersion,
-//   // useCdn: true,
-//   // token: process.env.SANITY_API_READ_TOKEN,
-//   useCdn: false, // set false if you need fresh data with token
-//   // token,
-// });
-
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
-  // useCdn: true,
-  // token: process.env.SANITY_API_READ_TOKEN,
+
   useCdn: false, // set false if you need fresh data with token
-  // token,
 });
