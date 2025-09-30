c6d124e2e673e64d66371350a1ee08bb8a9eda9b
naveenbishwas <naveenbishwas4@gmail.com>
2025-09-30 10:44:58 +0530

    env.js

--- src/sanity/lib/client.js
+++ src/sanity/lib/client.js
@@ -1,7 +1,7 @@
 import { createClient } from "next-sanity";
+import { apiVersion, dataset, projectId } from "../env.js";
 
-import { apiVersion, dataset, projectId } from "../env";
-console.log(projectId, "Naveen ProjectId");
+console.log("Sanity projectId:", projectId, "dataset:", dataset);
 
 export const client = createClient({
   projectId,
