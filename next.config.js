e55b50c0e9402563b656a27f8decc91c140fe5ce
naveenbishwas <naveenbishwas4@gmail.com>
2025-09-30 10:58:38 +0530

    projectId issue

--- next.config.js
+++ next.config.js
@@ -6,3 +6,13 @@ module.exports = {
     domains: ["cdn.sanity.io"],
   },
 };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  },
+};
+
+module.exports = nextConfig;
