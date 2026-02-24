1ec090491b9c9dcbd00823744bf07a40a0f69440
yashpapa6969 <yashgupta.yg856@gmail.com>
2026-02-24 14:47:37 +0530

    fix: Remove ESLint build ignore and add client directives
    - Remove eslint.ignoreDuringBuilds configuration from next.config.js
    - Add "use client" directive to ContactForm component for client-side rendering
    - Add "use client" directive to Header component for client-side rendering
    - Enforce proper build linting and ensure components are marked as client components in Next.js App Router

--- src/app/Components/ContactForm/page.js
+++ src/app/Components/ContactForm/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useRef, useState, useEffect } from "react";
 import "./contactForm.css";
 import { countryList } from "../../utils/countryList";
--- src/app/Components/Header/page.js
+++ src/app/Components/Header/page.js
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useState } from "react";
 import "./header.css";
 import Image from "next/image";
